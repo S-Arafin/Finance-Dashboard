a54c9b586f9d45215d3db118b93f1edef2152061
S-Arafin <arafin23103@gmail.com>
2026-04-02 19:05:57 +0600

    Project and router setup

---
+++ src/pages/Summary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const summary = () => {
+    return (
+        <div>
+            summary
+        </div>
+    );
+};
+
+export default summary;
---
+++ src/routes/router.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router";
+import mainLayout from "../Layouts/mainLayout";
+import ErrorPage from "../Error/ErrorPage";
+import summary from "../pages/summary";
+import Statement from "../pages/Statement";
+import Income from "../pages/Income";
+import Expenses from "../pages/Expenses";
+import User from "../pages/User";
+
+const router = createBrowserRouter(
+    [
+        {
+            path:'/',
+            Component: mainLayout,
+            errorElement: <ErrorPage/>,
+            children: [
+                {
+                    path: '/',
+                    Component: summary,
+                },
+                {
+                    path: '/statement',
+                    Component: Statement
+                },
+                {
+                    path: '/income',
+                    Component: Income,
+                },
+                {
+                    path: '/expenses',
+                    Component: Expenses,
+                },
+                {
+                    path: '/User',
+                    Component: User,
+                },
+                
+            ]
+            
+        }
+    ]
+)
+
+export default router;
